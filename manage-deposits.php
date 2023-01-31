9eff6f901bb8447ba7d3a0d1ad0780f62ad2bc4f
Vivek Verma <bookyourown.mutual@gmail.com>
2023-01-31 22:37:03 +0530

    jsjjs

--- manage-deposits.php
+++ manage-deposits.php
@@ -1,3 +1,64 @@
 <?php include "header.php";?>
 
+<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <div class="p-4 bg-white light:bg-gray-900">
+        <label for="table-search" class="sr-only">Search</label>
+        <div class="relative mt-1">
+            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <svg class="w-5 h-5 text-gray-500 light:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+            </div>
+            <input type="text" id="table-search" class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 light:bg-gray-700 light:border-gray-600 light:placeholder-gray-400 light:text-white light:focus:ring-blue-500 light:focus:border-blue-500" placeholder="Search for records by order-id">
+        </div>
+    </div>
+    <table class="w-full text-sm text-left text-gray-500 light:text-gray-400">
+        <thead class="text-gray-700 bg-gray-50 light:bg-gray-700 light:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3 text-left">
+                    Order-Id
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    User
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Amount
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Status
+                </th>
+                <th scope="col" class="px-6 py-3 text-right">
+                    Date
+                </th>
+                <!-- <th scope="col" class="px-6 py-3 text-right">
+                    Action
+                </th> -->
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            for($i = 0; $i < 10; $i++){ ?>
+                <tr class="bg-white border-b light:bg-gray-800 light:border-gray-700 hover:bg-gray-50 light:hover:bg-gray-600">
+                    <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap light:text-white">
+                        1
+                    </td>
+                    <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap light:text-white">
+                        Vivek Verma, +91 8653826902, Paytm Payments Banks
+                    </td>
+                    <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap light:text-white">
+                        ₹ 30.00
+                    </td>
+                    <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap light:text-white">
+                        Success
+                    </td>
+                    <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap light:text-white text-right">
+                        2023-06-19 12:03:55
+                    </td>
+                    <!-- <td class="px-6 py-4 text-right">
+                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                    </td> -->
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</div>
+
 <?php include "footer.php";?>
--- push-notification.php
+++ push-notification.php
@@ -3,15 +3,15 @@
 <form>
     <div class="grid md:grid-cols-2 md:gap-6">
         <div class="relative z-0 w-full mb-6 group">
-            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-            <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 light:bg-gray-700 dark:border-gray-600 light:placeholder-gray-400 light:text-white light:focus:ring-blue-500 light:focus:border-blue-500" placeholder="name@flowbite.com" required>
+            <label for="heading" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Heading</label>
+            <input type="text" id="heading" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 light:bg-gray-700 light:border-gray-600 light:placeholder-gray-400 light:text-white light:focus:ring-blue-500 light:focus:border-blue-500" placeholder="Heading" required>
         </div>
         <div class="relative z-0 w-full mb-6 group">
-            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-            <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 light:bg-gray-700 dark:border-gray-600 light:placeholder-gray-400 light:text-white light:focus:ring-blue-500 light:focus:border-blue-500" placeholder="name@flowbite.com" required>
+            <label for="content" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Message Content</label>
+            <textarea id="content" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 light:bg-gray-700 light:border-gray-600 light:placeholder-gray-400 light:text-white light:focus:ring-blue-500 light:focus:border-blue-500" placeholder="Type content here ...."></textarea>
         </div>
     </div>
-    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Send Notification</button>
 </form>
 
 <?php include "footer.php";?>
