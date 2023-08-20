d259ccb0949f412ca699682e9926b789256ea500
Vivek Verma <vivekdgp01@gmail.com>
2023-08-20 13:14:24 +0530

    Database Added

---
+++ database/sm_plays.sql
@@ -0,0 +1,155 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 20, 2023 at 09:44 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sm_plays`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+-- Error reading structure for table sm_plays.admin: #1932 - Table 'sm_plays.admin' doesn't exist in engine
+-- Error reading data for table sm_plays.admin: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sm_plays`.`admin`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `app_management`
+--
+-- Error reading structure for table sm_plays.app_management: #1932 - Table 'sm_plays.app_management' doesn't exist in engine
+-- Error reading data for table sm_plays.app_management: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sm_plays`.`app_management`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bank_details`
+--
+-- Error reading structure for table sm_plays.bank_details: #1932 - Table 'sm_plays.bank_details' doesn't exist in engine
+-- Error reading data for table sm_plays.bank_details: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sm_plays`.`bank_details`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bid_results`
+--
+-- Error reading structure for table sm_plays.bid_results: #1932 - Table 'sm_plays.bid_results' doesn't exist in engine
+-- Error reading data for table sm_plays.bid_results: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sm_plays`.`bid_results`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `double_patti_bids`
+--
+-- Error reading structure for table sm_plays.double_patti_bids: #1932 - Table 'sm_plays.double_patti_bids' doesn't exist in engine
+-- Error reading data for table sm_plays.double_patti_bids: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sm_plays`.`double_patti_bids`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `full_sangam_bids`
+--
+-- Error reading structure for table sm_plays.full_sangam_bids: #1932 - Table 'sm_plays.full_sangam_bids' doesn't exist in engine
+-- Error reading data for table sm_plays.full_sangam_bids: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sm_plays`.`full_sangam_bids`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `game_rule`
+--
+-- Error reading structure for table sm_plays.game_rule: #1932 - Table 'sm_plays.game_rule' doesn't exist in engine
+-- Error reading data for table sm_plays.game_rule: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sm_plays`.`game_rule`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `half_sangam_bids`
+--
+-- Error reading structure for table sm_plays.half_sangam_bids: #1932 - Table 'sm_plays.half_sangam_bids' doesn't exist in engine
+-- Error reading data for table sm_plays.half_sangam_bids: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sm_plays`.`half_sangam_bids`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jodi_bids`
+--
+-- Error reading structure for table sm_plays.jodi_bids: #1932 - Table 'sm_plays.jodi_bids' doesn't exist in engine
+-- Error reading data for table sm_plays.jodi_bids: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sm_plays`.`jodi_bids`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifications`
+--
+-- Error reading structure for table sm_plays.notifications: #1932 - Table 'sm_plays.notifications' doesn't exist in engine
+-- Error reading data for table sm_plays.notifications: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sm_plays`.`notifications`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `passbook`
+--
+-- Error reading structure for table sm_plays.passbook: #1932 - Table 'sm_plays.passbook' doesn't exist in engine
+-- Error reading data for table sm_plays.passbook: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sm_plays`.`passbook`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `satta_markets`
+--
+-- Error reading structure for table sm_plays.satta_markets: #1932 - Table 'sm_plays.satta_markets' doesn't exist in engine
+-- Error reading data for table sm_plays.satta_markets: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sm_plays`.`satta_markets`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `single_ank_bids`
+--
+-- Error reading structure for table sm_plays.single_ank_bids: #1932 - Table 'sm_plays.single_ank_bids' doesn't exist in engine
+-- Error reading data for table sm_plays.single_ank_bids: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sm_plays`.`single_ank_bids`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `single_patti_bids`
+--
+-- Error reading structure for table sm_plays.single_patti_bids: #1932 - Table 'sm_plays.single_patti_bids' doesn't exist in engine
+-- Error reading data for table sm_plays.single_patti_bids: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sm_plays`.`single_patti_bids`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `triple_patti_bids`
+--
+-- Error reading structure for table sm_plays.triple_patti_bids: #1932 - Table 'sm_plays.triple_patti_bids' doesn't exist in engine
+-- Error reading data for table sm_plays.triple_patti_bids: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sm_plays`.`triple_patti_bids`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+-- Error reading structure for table sm_plays.user: #1932 - Table 'sm_plays.user' doesn't exist in engine
+-- Error reading data for table sm_plays.user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `sm_plays`.`user`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
